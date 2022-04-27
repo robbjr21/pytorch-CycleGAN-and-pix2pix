68490ef4b5f3b97ee1eafb41fa8b4d5478c47261
Rob Baldoni <robert.baldoni@duke.edu>
2022-04-26 20:24:09 -0400

    Tweaking model

--- main.sh
+++ main.sh
@@ -1,2 +1,2 @@
-cp /checkpoints/testingRuns/latest_net_G_A.pth /checkpoints/testingRuns/latest_net_G.pth
-python test.py --dataroot datasets/testingRuns/testA --name checkpoints/testingRuns --model test --no_dropout --use_wandb
+cp checkpoints/testingRuns/latest_net_G_A.pth ./checkpoints/testingRuns/latest_net_G.pth
+python test.py --dataroot datasets/testingRuns/testA --name checkpoints/testingRuns --model test --gpu_ids '-1' --no_dropout
