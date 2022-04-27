2c4fdb11f944e5ae5a990df638f68fb9695081c2
Rob Baldoni <robert.baldoni@duke.edu>
2022-04-27 19:46:51 -0400

    updates on app

--- streamlitapp.py
+++ streamlitapp.py
@@ -3,18 +3,18 @@ from PIL import Image
 # streamlit run app.py
 st.set_option("deprecation.showfileUploaderEncoding", False)
 
-st.title("Language Detection from Document Image Demo (AIPI540 CV Module)")
+st.title("MRI to CT Translation (AIPI540 Final Project)")
 st.write("")
 
 file_up = st.file_uploader("Upload an image", type="jpg")
 
 
-import subprocess
-subprocess.call(["main.sh"])
+#import subprocess
+#subprocess.call(["main.sh"])
 
 if file_up is not None:
     image = Image.open(file_up)
-    st.image(image, caption="Uploaded Image.", use_column_width=True)
+    st.image(image, caption="Uploaded MRI Image", use_column_width=True)
     st.write("")
     st.write("Just a second...")
-    labels = predict(file_up)
+    st.image(results/checkpoints/testingRuns/test_latest/images/mri1440_fake.png, caption="Fake CT Image", use_column_width=True)
