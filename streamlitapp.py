665c4c82f0f137a6c6cd6caf65ef514a38456462
Rob Baldoni <robert.baldoni@duke.edu>
2022-04-27 21:52:30 -0400

    Edits

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from PIL import Image
+import os
+import subprocess
+
 # streamlit run app.py
 st.set_option("deprecation.showfileUploaderEncoding", False)
 
@@ -8,13 +11,16 @@ st.write("")
 
 file_up = st.file_uploader("Upload an image", type="jpg")
 
-
-#import subprocess
-#subprocess.call(["main.sh"])
+subprocess.call(["main.sh"])
 
 if file_up is not None:
+    with open(os.path.join("datasets/testingRuns/testA",file_up.name),"wb") as f:
+        f.write(file_up.getbuffer())
+    subprocess.call(['sh', './main.sh'])
     image = Image.open(file_up)
     st.image(image, caption="Uploaded MRI Image", use_column_width=True)
     st.write("")
     st.write("Just a second...")
-    st.image(results/checkpoints/testingRuns/test_latest/images/mri1440_fake.png, caption="Fake CT Image", use_column_width=True)
+    name_ext = file_up.name + "_fake"
+    st.write(name_ext)
+    st.image('results/checkpoints/testingRuns/test_latest/images/mri1440_fake.png', caption="Fake CT Image", use_column_width=True)
